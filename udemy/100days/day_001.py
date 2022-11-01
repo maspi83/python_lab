a8f3b7c9a9c00b74af4fc74160e924a5b4be4ecc
Martin Spilovsky <martin.spilovsky@gmail.com>
2022-11-01 19:34:05 +0100

    update

--- udemy/100days/day_001.py
+++ udemy/100days/day_001.py
@@ -1,3 +1,12 @@
-a = "debil"
-#dlzkaa=(len(a))
+###
+a = "curak"
+###
+print ("Varianta 1")
 print ("Meno: " + '"'+a+'"' + " dlzka: " + str(len(a)) )
+
+print ("Varianta 2")
+dlzkaa=len(a)
+print ("Meno: " + '"'+a+'"' + " dlzka: " + str(dlzkaa) )
+
+print ("Varianta 3: input test")
+print ("Meno: " + input("Ako sa volas: "))
